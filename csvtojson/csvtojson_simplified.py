18302d57ce1f75e63f2ef1aa3a94d354e9068a1d
abrambhanvaji <abram.bhanvaji@gmail.com>
2022-08-21 14:37:51 +0700

    20220821-commit2

--- csvtojson/csvtojson_simplified.py
+++ csvtojson/csvtojson_simplified.py
@@ -32,14 +32,34 @@ colnames = ['id',
 'is_doc_completed']
 data = pd.read_csv(datapath, names=colnames, header=None)
 
-# Take all dates and reformat with a timezone
-date_split = re.split('[- :]', data['date_created'][0])
-date_prep = []  
-for i in date_split:
-    date_prep.append(int(i))
-date_reformat = datetime.datetime(date_prep[0], date_prep[1], date_prep[2], date_prep[3], date_prep[4], date_prep[5])
-date_reformatted = date_reformat.strftime('%Y-%m-%dT%H:%M:%SZ')
-print(date_reformatted)
+# Function reformat_date():
+def reformat_date(date_column):
+    new_date = []
+
+    for x in date_column:
+        date_split = re.split('[- :]', x)
+        date_prep = []
+        
+        if len(date_split) == 6:
+            for i in date_split:
+                date_prep.append(int(i))
+            date_reformat = datetime.datetime(date_prep[0], date_prep[1], date_prep[2], date_prep[3], date_prep[4], date_prep[5])
+            date_reformatted = date_reformat.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+            new_date.append(date_reformatted)
+        elif len(date_split) == 3:
+            for i in date_split:
+                date_prep.append(int(i))
+            date_reformat = datetime.datetime(date_prep[0], date_prep[1], date_prep[2])
+            date_reformatted = date_reformat.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+            new_date.append(date_reformatted)
+
+    date_column = new_date
+    return date_column
+
+# Take all date columns and reformat
+data['date_created'] = reformat_date(data['date_created'])
+data['date_last_modified'] = reformat_date(data['date_last_modified'])
+data['active_date'] = reformat_date(data['active_date'])
 
 # Take all phone numbers and prepend a '+' string
 data['phone'] = "+" + data['phone'].astype(str)
